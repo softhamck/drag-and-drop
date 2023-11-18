195839773b17f35339c3ba923866beff55910c78
softhamck <andreamunozcano5@gmail.com>
2023-11-18 08:54:30 -0500

    Comentarios a los componentes Padre e Hijo

--- src/components/Hijo/index.jsx
+++ src/components/Hijo/index.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 
+// Definición del componente Hijo como una función que recibe propiedades
 const Hijo = ({ nombre, onDrop, onDragOver }) => {
+  // Función para manejar el clic en el botón
   const handleClick = () => {
-    console.log(`Hola Mundo desde ${nombre}`);
+    console.log(`Hola Mundo desde ${nombre}`); // Imprime un mensaje en la consola con el nombre del hijo
   };
 
+  // Función para manejar el inicio del arrastre de un hijo
   const handleDragStart = (e) => {
-    e.dataTransfer.setData("childName", nombre);
+    e.dataTransfer.setData("childName", nombre); // Guarda el nombre del hijo en los datos de transferencia
   };
 
+  // Renderizado del componente Hijo
   return (
     <div
-      className="hijo"
-      draggable
-      onDragStart={handleDragStart}
-      onDrop={onDrop}
-      onDragOver={onDragOver}
+      className="hijo" // Clase CSS del elemento div
+      draggable // Permite que el elemento sea arrastrable
+      onDragStart={handleDragStart} // Maneja el evento de inicio de arrastre
+      onDrop={onDrop} // Maneja el evento de soltar un hijo
+      onDragOver={onDragOver} // Maneja el evento de arrastrar sobre un hijo
     >
-      <button onClick={handleClick}>Mostrar Hola Mundo en {nombre}</button>
+      <button onClick={handleClick}>Mostrar Hola Mundo en {nombre}</button>{" "}
+      {/* Botón que muestra el nombre del hijo */}
     </div>
   );
 };
--- src/components/Padre/padre.jsx
+++ src/components/Padre/padre.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Hijo from "../Hijo/index";
 
+// Definición del componente Padre como una función
 const Padre = () => {
+  // Estado local para almacenar los nombres de los hijos
   const [children, setChildren] = useState([
     "Alice",
     "Bob",
@@ -10,28 +12,32 @@ const Padre = () => {
     "Eve",
   ]);
 
+  // Función para manejar el evento de soltar un elemento
   const handleDrop = (e, index) => {
-    e.preventDefault();
-    const childName = e.dataTransfer.getData("childName");
-    const newChildren = [...children];
-    const childIndex = newChildren.indexOf(childName);
-    newChildren.splice(childIndex, 1);
-    newChildren.splice(index, 0, childName);
-    setChildren(newChildren);
+    e.preventDefault(); // Evita el comportamiento predeterminado del navegador al soltar un elemento
+    const childName = e.dataTransfer.getData("childName"); // Obtiene el nombre del hijo arrastrado
+    const newChildren = [...children]; // Crea una copia del array de nombres
+    const childIndex = newChildren.indexOf(childName); // Obtiene el índice del hijo arrastrado
+    newChildren.splice(childIndex, 1); // Elimina el hijo arrastrado del array
+    newChildren.splice(index, 0, childName); // Inserta el hijo en la nueva posición
+    setChildren(newChildren); // Actualiza el estado con la nueva lista de hijos
   };
 
+  // Función para permitir que un elemento sea soltado en este contenedor
   const allowDrop = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Evita el comportamiento predeterminado del navegador al arrastrar elementos
   };
 
+  // Renderizado del componente Padre
   return (
     <div className="padre">
+      {/* Mapeo de los nombres de los hijos para renderizar componentes Hijo */}
       {children.map((nombre, index) => (
         <Hijo
-          key={index}
-          nombre={nombre}
-          onDrop={(e) => handleDrop(e, index)}
-          onDragOver={allowDrop}
+          key={index} // Propiedad key para identificar de manera única cada hijo en la lista
+          nombre={nombre} // Propiedad nombre pasada al componente Hijo
+          onDrop={(e) => handleDrop(e, index)} // Propiedad que maneja el evento de soltar un hijo
+          onDragOver={allowDrop} // Propiedad que maneja el evento de arrastrar sobre un hijo
         />
       ))}
     </div>
